0a88bc9e0bcfdb7699f43397aa0adf47ac09c385
Damien Lim <damienlim@Damiens-MacBook-Pro.local>
2025-08-20 14:58:08 +0800

    finished level 1

--- src/main/java/Tension.java
+++ src/main/java/Tension.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Tension {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Tension\n" +
                 "What can I do for you?");
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                scanner.close();
+                break;
+            }
+            System.out.println(command);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
