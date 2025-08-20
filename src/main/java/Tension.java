32635954b1ddc1e108bb2dae07590102a0adf3a9
Damien Lim <damienlim@Damiens-MacBook-Pro.local>
2025-08-20 15:11:10 +0800

    finished level 2

--- src/main/java/Tension.java
+++ src/main/java/Tension.java
@@ -3,16 +3,27 @@ import java.util.Scanner;
 public class Tension {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] inputs = new String[100];
+        int counter = 0;
         System.out.println("Hello! I'm Tension\n" +
                 "What can I do for you?");
         while (true) {
             String command = scanner.nextLine();
-            if (command.equals("bye")) {
-                scanner.close();
-                break;
+            switch (command) {
+                case "list":
+                    for (int i = 0; i < counter; i++) {
+                        System.out.println(i+1 + ". " + inputs[i]);
+                    }
+                    break;
+                case "bye":
+                    scanner.close();
+                    System.out.println("Bye. Hope to see you again soon!");
+                    return;
+                default:
+                    System.out.println(command);
+                    inputs[counter++] = command;
+                    break;
             }
-            System.out.println(command);
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
