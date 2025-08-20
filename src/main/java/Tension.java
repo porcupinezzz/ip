89996ba12137eaabb5b3ce47569a468f8182ad55
Damien Lim <damienlim@Damiens-MacBook-Pro.local>
2025-08-20 14:39:21 +0800

    completed level 0

---
+++ src/main/java/Tension.java
@@ -0,0 +1,7 @@
+public class Tension {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Tension\n" +
+                "What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
