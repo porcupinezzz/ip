26813708081d99decf814c4aa77238ebdba3158d
Damien Lim <damienlim@Damiens-MacBook-Pro-3.local>
2025-09-13 05:26:16 +0800

    improve quality of code and remove unnecessary methods
    There was some arrowhead style code and code blocks that were not
    in use
    
    Both those factors make it hard to read code when needed
    
    Delete unused methods and abstract code to functions
    
    It reduces the total lines of code and improves readability

--- src/main/java/tension/helper/TaskList.java
+++ src/main/java/tension/helper/TaskList.java
@@ -40,74 +40,6 @@ public class TaskList {
      * Executes all the commands in the command list
      * and adds them to the task list
      */
-    public boolean executesAndExits() throws TensionException {
-        int counter = tasks.size();
-        Command command = commands.remove(0);
-        int index = command.getIndex();
-        switch (command.getCommandName()) {
-        case "find":
-            ArrayList<Task> foundTasks = new ArrayList<>();
-            for (Task task : tasks) {
-                String keyword = command.getFullWord().split(" ")[1];
-                String[] taskKeywords = (task.getDescription().split(" "));
-                for (String taskKeyword : taskKeywords) {
-                    if (taskKeyword.equals(keyword)) {
-                        foundTasks.add(task);
-                        break;
-                    }
-                }
-            }
-            ui.listMatchingTasks(foundTasks);
-            break;
-        case "delete":
-            Task removed = tasks.remove(index);
-            storage.deleteLine(index);
-            counter--;
-            ui.displayDeletedTask(removed, counter);
-            break;
-        case "event": // Fallthrough
-        case "deadline": // Fallthrough
-        case "todo":
-            Task currTask;
-            try {
-                currTask = Task.makeTask(command.getFullWord());
-            } catch (Exception e) {
-                e.toString();
-                e.printStackTrace();
-                return false;
-            }
-            tasks.add(currTask);
-            storage.writeFile(currTask);
-            counter++;
-            ui.displayAddedTask(currTask, counter);
-            break;
-        case "mark": // Fallthrough
-        case "unmark":
-            if (index < 0 || index >= counter) {
-                System.out.println("Invalid index");
-            } else {
-                Task task = tasks.get(index);
-                String s = task.getStatus(command.getIsMarked());
-                storage.rewriteLine(index, task.makeStoreString());
-                System.out.println(s);
-            }
-            break;
-        case "list":
-            ui.listTasks(tasks, counter);
-            break;
-        case "bye":
-            System.out.println("Bye. Hope to see you again soon!");
-            return true;
-        default:
-            throw new TensionException("Invalid command, please start with todo, deadline, or event");
-        }
-        return false;
-    }
-
-    /**
-     * Executes all the commands in the command list
-     * and adds them to the task list
-     */
     public String executesAndReturnString() throws TensionException {
         int counter = tasks.size();
         Command command = commands.remove(0);
@@ -115,18 +47,8 @@ public class TaskList {
         switch (command.getCommandName()) {
         case "find":
             ArrayList<Task> foundTasks = new ArrayList<>();
-            for (Task task : tasks) {
-                String keyword = command.getFullWord().split(" ")[1];
-                String[] taskKeywords = (task.getDescription().split(" "));
-                for (String taskKeyword : taskKeywords) {
-                    if (taskKeyword.equals(keyword)) {
-                        foundTasks.add(task);
-                        break;
-                    }
-                }
-            }
-            ui.listMatchingTasksAsString(foundTasks);
-            break;
+            Task.findTasksWithKeyword(tasks, foundTasks, command.getFullWord());
+            return ui.listMatchingTasksAsString(foundTasks);
         case "delete":
             Task removed = tasks.remove(index);
             storage.deleteLine(index);
@@ -162,6 +84,5 @@ public class TaskList {
         default:
             return "Invalid command, please start with todo, deadline, or event";
         }
-        return "";
     }
 }
